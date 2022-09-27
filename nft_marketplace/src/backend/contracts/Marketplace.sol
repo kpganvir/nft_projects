a3fd6d9f071a064d482a08c2a7da4638e7562cac
kpganvir <kalps.ganvir@gmail.com>
2022-09-27 21:37:33 +0530

    adding test cases for makeItem function in marketplace

--- nft_marketplace/src/backend/contracts/Marketplace.sol
+++ nft_marketplace/src/backend/contracts/Marketplace.sol
@@ -13,13 +13,60 @@ contract Marketplace is ReentrancyGuard
 
     address  payable public immutable feeAccount;//receive fees
     uint public immutable feePercent;//fee % on sales
+    uint public itemCount=0;
+    struct Item{
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
 
+    }
+    mapping(uint => Item) public items;
+
+   event  offered(
+        uint itemId,
+        address indexed nft,
+        uint tokenId,
+        uint price,
+        address indexed seller
+       
+   );
 
     constructor(uint _feePercent)
     {
 
         feeAccount=payable(msg.sender);
         feePercent=_feePercent;
+    }
+
+    function  makeItem(IERC721 _nft,uint _tokenId,uint _price) external nonReentrant 
+    {
+        require(_price >0,"Price must be greater than zero");
+        itemCount++; 
+         _nft.transferFrom(msg.sender,address(this),_tokenId);
+
+         //initialize new Item and add it in mapping
+
+         items[itemCount]= Item({
+            itemId: itemCount,
+            nft:_nft,
+           tokenId: _tokenId,
+            price: _price,
+            seller: payable(msg.sender),
+            sold: false
+         });
+   //mint this token
+          emit offered(
+              itemCount,
+            address(_nft),
+            _tokenId,
+            _price,
+            msg.sender
+           
+          );
+ 
     }
 
 }
--- nft_marketplace/src/backend/test/NFTMarketplace.test.js
+++ nft_marketplace/src/backend/test/NFTMarketplace.test.js
@@ -1,4 +1,6 @@
 const {expect }=require("Chai");
+const toWei =(num)=>ethers.utils.parseEther(num.toString());//ether to wei
+const fromWei=(num)=>ethers.utils.formatEther(num); //put one decimal point
 
  describe("NFTMarketplace",async function(){
 
@@ -37,9 +39,57 @@ const {expect }=require("Chai");
         const _balanceOf=await nft.balanceOf(add1.address)
         expect(Number(_tokenCount)).to.equal(1);
         expect(Number(_balanceOf)).to.equal(1);
-        expect(await nft.tokenURI(1)).to.equal(URI);
+       expect(await nft.tokenURI(1)).to.equal(URI);
+
+
+        await nft.connect(add2).mint(URI);
+        const _tokenCount2=await nft.tokenCount();
+        const _balanceOf2=await nft.balanceOf(add2.address)
+      expect(Number(_tokenCount2)).to.equal(2);
+       expect(Number(_balanceOf2)).to.equal(1);
+       expect(await nft.tokenURI(2)).to.equal(URI);
      })
 
    })
+
+   describe("Making marketplace item NFT",function(){
+
+      beforeEach( async function(){
+         ///add1 mints an nft
+         await nft.connect(add1).mint(URI);
+         await nft.connect(add1).setApprovalForAll(marketplace.address,true);
+   
+      })
+   
+      it( "should track newly created item,transafer NFT from seller to marketplace and emit offered event",
+         async function(){
+
+            await expect(marketplace.connect(add1).makeItem(nft.address,1,toWei()))
+            .to.emit(marketplace,"offered")
+            .withArgs(1,nft.address,1,toWei(),add1.address)
+
+            expect(await nft.ownerOf()).to.equal(marketplace.address);
+            expect(await marketplace.itemCount()).to.equal(1);
+
+            const item= await marketplace.items(1);
+            expect(item.itemId).to.equal(1);
+            expect(item.nft).to.equal(nft.address);
+            expect(item.tokenId).to.equal(1);
+            expect(item.price).to.equal(toWei(1));
+            expect(item.sold).to.equal(false);
+
+         });
+
+        
+      
+   
+
+
+   // });
+
+  
+  
+
+
 
 })
