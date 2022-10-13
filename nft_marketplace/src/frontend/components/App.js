d672b079c7eac4344ca41904e300304e8c376d84
kpganvir <kalps.ganvir@gmail.com>
2022-10-13 23:10:30 +0530

    adding Frontend

--- nft_marketplace/src/frontend/components/App.js
+++ nft_marketplace/src/frontend/components/App.js
@@ -17,31 +17,32 @@ import { ethers } from "ethers"
 import { Spinner } from 'react-bootstrap'
 
 import './App.css';
+import {ethers }from "ethers";
 
 function App() {
+
+  const [account,setAccount]=useState(null);
+
+  //connecting to metamast/login
+const web3Handler = async()=>{
+
+  //get all accounts from wallet
+  const accounts =await window.ethereum.request({method: 'eth_requestAccounts'}) ;
+  setAccount(accounts[0]);
+  //get provider obj
+  const provider =new ethers.providers.Web3Provider(window.ethereum);//
+  const signer =provider.getSigner();
+  loadContracts(signer);
+}
+
+
+
+
   const [loading, setLoading] = useState(true)
-  const [account, setAccount] = useState(null)
+ 
   const [nft, setNFT] = useState({})
   const [marketplace, setMarketplace] = useState({})
-  // MetaMask Login/Connect
-  const web3Handler = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    setAccount(accounts[0])
-    // Get provider from Metamask
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    // Set signer
-    const signer = provider.getSigner()
-
-    window.ethereum.on('chainChanged', (chainId) => {
-      window.location.reload();
-    })
-
-    window.ethereum.on('accountsChanged', async function (accounts) {
-      setAccount(accounts[0])
-      await web3Handler()
-    })
-    loadContracts(signer)
-  }
+ 
   const loadContracts = async (signer) => {
     // Get deployed copies of contracts
     const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer)
